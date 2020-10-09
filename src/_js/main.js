559ab64ce154e0d516d5bf77b7f5387d8fc0ee78
Braden East <bradeneastdesign@gmail.com>
2020-10-09 11:44:15 -0500

    mostly style updates

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,15 +1,31 @@
 import Schwifty from './schwifty.js';
 
+/**@returns {HTMLElement} */
 let $ = (selector, context = document) => context.querySelector(selector);
 let $$ = (selector, context = document) => context.querySelectorAll(selector);
+let ls = (key, value) => value == undefined
+  ? JSON.parse(localStorage.getItem(key))
+  : localStorage.setItem(key, JSON.stringify(value));
+let toggleGlobalClass = (className, force) => document.documentElement.classList.toggle(className, force);
 
 function init() {
+  toggleGlobalClass('dark', ls('dark'));
+
+  $('#theme-toggle').addEventListener('click', () => {
+    let isDark = ls('dark');
+    toggleGlobalClass('dark', !isDark);
+    ls('dark', !isDark);
+  })
+
   $$('nav a').forEach(a => {
     if (a.href == location.href)
       a.setAttribute('aria-current', true);
   })
 }
 
+init();
+
 new Schwifty({
-  onload: init
+  onload: init,
+  preserveAttributes: true
 })
